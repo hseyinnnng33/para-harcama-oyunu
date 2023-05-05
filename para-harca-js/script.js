05d00bfc87b491e800815d4e78a57286a6fd6959
Hüseyin Aydın <110617469+hseyinnnng33@users.noreply.github.com>
2023-05-05 18:43:04 +0300

    Add files via upload

---
+++ para-harca-js/script.js
@@ -0,0 +1,61 @@
+const adetBtn = document.querySelectorAll(".al-urun");
+const silBtn = document.querySelectorAll(".sil-urun");
+const kalanpara = document.querySelector(".kalan-para-span");
+const btngorunmez = document.querySelectorAll(".sil-urun1");
+
+
+let cikar = 0;
+let donusturKalan = Number(kalanpara.textContent);
+kalanpara.innerHTML = donusturKalan.toLocaleString();
+
+adetBtn.forEach((item)=>{
+    item.addEventListener("click", function(){
+        let bir =  item.getAttribute("data-id");
+        cikar = donusturKalan - Number(bir);
+        if(donusturKalan>=bir){
+        donusturKalan = cikar;
+        kalanpara.innerHTML = `${donusturKalan.toLocaleString()}`;
+        let inputAdet = item.parentNode.querySelector('.input-adet');
+        inputAdet.innerHTML = Number(inputAdet.innerHTML) + 1;
+        if(inputAdet>=0){
+        item.classList.remove("active")
+        }
+        }
+        else{
+            alert("HAHAHHAAH FAKİR PARAN YETMİYOR")
+        }
+    })
+})
+
+silBtn.forEach((item)=>{
+    item.addEventListener("click", function(){
+        let bir =  item.getAttribute("data-id");
+        cikar = donusturKalan+ Number(bir);   // 1
+        donusturKalan = cikar;  // 2
+        let inputAdet = item.parentNode.querySelector('.input-adet');
+        let adet = parseInt(inputAdet.innerHTML) - 1;
+        if(adet>=0){
+        kalanpara.innerHTML = `${donusturKalan.toLocaleString()}`;
+        inputAdet.innerHTML = adet;
+        item.classList.add("active");
+        }
+        else{
+            item.classList.add("active")
+        }
+    })
+})
+
+const kalanParaDiv = document.querySelector('.kalan-para');
+const initialOffsetTop = kalanParaDiv.offsetTop;
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset >= initialOffsetTop) {
+    kalanParaDiv.style.position = 'fixed';
+    kalanParaDiv.style.top = '0';
+    // kalanParaDiv.style.left = '21%';
+    kalanParaDiv.classList.add("active")
+    kalanParaDiv.scrollIntoView({behavior: "smooth"})
+  } else {
+    kalanParaDiv.style.position = 'static';
+  }
+});
